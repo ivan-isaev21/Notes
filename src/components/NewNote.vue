b89da325c3b2667074b1eaa55badf6e28ef6cf9c
ivan-isaev21 <isaev.ivan.aleksandrovich@gmail.com>
2022-06-03 16:49:32 +0300

    Create note project

---
+++ src/components/NewNote.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="new-note">
+        <label>Title</label>
+        <input v-model="note.title" type="text">
+        <label>Description</label>
+        <textarea v-model="note.descr"></textarea>
+        <label>Priority</label>
+        <select v-model="note.priority">
+            <option value="default">default</option>
+            <option value="warning">warning</option>
+            <option value="danger">danger</option>
+        </select>
+
+        <button class="btn btnPrimary" style="margin-top:2rem;" @click="addNote">New note</button>
+    </div>
+</template>
+<script>
+    export default {
+
+        props: {
+            note: {
+                type: Object,
+                required: true
+            }
+        },
+        methods: {
+            addNote() {
+                this.$emit('addNote', this.note);
+            }
+        }
+    }
+</script>
+<style lang="scss">
+    .new-note {
+        text-align: center;
+    }
+</style>
